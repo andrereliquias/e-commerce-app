9d5210cff33e2581ada33cc38627297c4aebfb6d
AndreReliquias <andrereliquias.contato@gmail.com>
2022-05-22 22:13:22 -0400

    Adiciona cliente do banco de dados

---
+++ src/shared/database.js
@@ -0,0 +1,51 @@
+var AWS = require('aws-sdk');
+
+let option = { region: 'us-east-1' };
+
+if (process.env.IS_OFFLINE || process.env.IS_TEST) {
+  option = {
+    region: process.env.IS_OFFLINE ? 'dynamo' : 'localhost',
+    endpoint: process.env.IS_OFFLINE ? 'http://dynamo:8000' : 'http://localhost:8000',
+    accessKeyId: 'DEFAULT_ACCESS_KEY',
+    secretAccessKey: 'DEFAULT_SECRET'
+  }
+}
+
+AWS.config.update(option);
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+const client = {
+  insert: (item, tableName) => {
+    const params = {
+      TableName: tableName,
+      Item: item
+    };
+
+    return ddb.put(params).promise();
+  },
+  read: (keys, tableName) => {
+    const params = {
+      TableName: tableName,
+      Key: keys
+    };
+
+    return ddb.get(params).promise();
+  },
+  query: (params) => {
+    return ddb.query(params).promise();
+  },
+  update: (params) => {
+    return ddb.update(params).promise();
+  },
+  delete: (keys, tableName) => {
+    const params = {
+      TableName: tableName,
+      Key: keys
+    };
+
+    return ddb.delete(params).promise();
+  }
+}
+
+module.exports = client;
